926370dc22027e30579f01cf08a8d0648639edae
Zonkel <joonakareinen@gmail.com>
2021-11-14 16:54:55 +0200

    update 4

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,26 +1,41 @@
 import MyList from "./MyList";
 import {useState} from "react"
 
+
 function MyContainer() {
     const [text, setText] = useState('')
     const header = "This is very important header"
     const [items, setItems] = useState([
         {id: "1",
-         text: "this is an item"},
+         text: "this is an item",
+        clicked: false},
         {id: "2",
-         text: "also this"}
+         text: "also this",
+         clicked: false}
       ])
 
     const addItem = (text) => {
-        const id = Math.floor(Math.random() * 1000000 + 1000)
+        const id = items.length + 1;
         const newItem = {id, ...text}
         setItems([...items, newItem])
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        addItem({text: text});
+        addItem({text: text, clicked: false});
         setText("");
+    }
+
+    const updateItem = (id) =>{
+        if(items[id-1].clicked === true){
+            items[id-1].clicked = false
+        }else{
+            items[id-1].clicked = true
+        }
+        setItems([...items])
+        
+        // document.getElementById(id).classList.add('myClass');
+        // items[id-1].text.classList.add("myClass");
     }
     
     
@@ -29,6 +44,7 @@ function MyContainer() {
     return (
         <div>
             <MyList 
+            updateItem={updateItem}
             header={header}
             items={items}/>
             <form onSubmit={onSubmit}>
